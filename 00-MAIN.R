a124f9548ab687c4b51d86b8511a91305ddd10ed
DamienHennom <hennom.damien@laposte.net>
2016-11-02 22:10:49 +0100

    Graph2

--- 00-MAIN.R
+++ 00-MAIN.R
@@ -36,17 +36,28 @@ live %>%
 live %>%
   group_by(country)%>%
   summarize(n= n()) %>%
-  mutate(n   = round(n/sum(n)*100,2))-> bla
+  mutate(n   = round(n/sum(n)*100,2))-> live_by_country
 
 
 highchart() %>% 
-  hc_add_series_labels_values(bla$country, 
-                              bla$n, 
+  hc_add_series_labels_values(live_by_country$country, 
+                              live_by_country$n, 
                               type = "pie",
                               colorByPoint = TRUE) %>%
-  hc_xAxis(categories = bla$country) %>%
+  hc_xAxis(categories = live_by_country$country) %>%
   hc_title(text = "Kickstart by country") %>%
   hc_legend(enabled = FALSE)
 
 
+library(treemap)
+data("GNI2014")
+GNI2014$iso2<-substr(GNI2014$iso3,1,2)
 
+live_by_country %>%
+  mutate(iso2 = as.character(live_by_country$country)) %>%
+  left_join(GNI2014,by=c("iso2"="iso2")) -> bla2
+
+
+highchart() %>% 
+  hc_add_series_map(worldgeojson, bla2,
+                    value = "n", joinBy = "iso3")
