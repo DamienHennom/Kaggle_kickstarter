74a7cb196393b674eb2abcb3e47d4bd04a16708e
DamienHennom <hennom.damien@laposte.net>
2016-11-04 00:01:21 +0100

    update

--- 01-DATA_MANIPULATION.R
+++ 01-DATA_MANIPULATION.R
@@ -12,16 +12,14 @@ live<-data.frame(read.csv(file = "00-BASE/live.csv"),row.names = TRUE)
 most_backed<-data.frame(read.csv(file = "00-BASE/most_backed.csv"),row.names = TRUE)
 
 #MAP
-pass<-data.frame(read.csv(file = "00-BASE/Matrix.csv",sep=";"))
-data(worldgeojson)
-
+#pass<-data.frame(read.csv(file = "00-BASE/Matrix.csv",sep=";"))
+data(worldgeojson,package="highcharter")
+worldgeojson<-worldgeojson
 
 #country
 most_backed$country<-unlist(lapply(strsplit(as.character(most_backed$location),
                                             split = ","),
                                    FUN = function(x) x[2]))
 
-bla<-as.data.frame(cbind(as.character(most_backed$location),most_backed$country))
-write.csv(bla,file="most_backed.csv")
 
 levels(unique(live$country))
