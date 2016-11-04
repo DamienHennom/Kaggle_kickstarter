5c5051ebd2a99bce3a1ed4e061898dfbe354cb57
DamienHennom <hennom.damien@laposte.net>
2016-11-04 21:10:41 +0100

    Update

--- 01-DATA_MANIPULATION.R
+++ 01-DATA_MANIPULATION.R
@@ -12,14 +12,5 @@ live<-data.frame(read.csv(file = "00-BASE/live.csv"),row.names = TRUE)
 most_backed<-data.frame(read.csv(file = "00-BASE/most_backed.csv"),row.names = TRUE)
 
 #MAP
-#pass<-data.frame(read.csv(file = "00-BASE/Matrix.csv",sep=";"))
 data(worldgeojson,package="highcharter")
-worldgeojson<-worldgeojson
 
-#country
-most_backed$country<-unlist(lapply(strsplit(as.character(most_backed$location),
-                                            split = ","),
-                                   FUN = function(x) x[2]))
-
-
-levels(unique(live$country))
--- 02-ANALYSE.R
+++ 02-ANALYSE.R
@@ -33,179 +33,44 @@ highchart() %>%
   hc_title(text = "Kickstarter by country") %>%
   hc_legend(enabled = FALSE)
 
-pass<-data.frame(iso2=c("AE",
-                        "AF",
-                        "AT",
-                        "AU",
-                        "AU",
-                        "BE",
-                        "BE",
-                        "BG",
-                        "BG",
-                        "BW",
-                        "BY",
-                        "CA",
-                        "CA",
-                        "CH",
-                        "CH",
-                        "CH",
-                        "CL",
-                        "CN",
-                        "CO",
-                        "CO",
-                        "CO",
-                        "CO",
-                        "CR",
-                        "CU",
-                        "CY",
-                        "CZ",
-                        "DE",
-                        "DK",
-                        "EC",
-                        "EE",
-                        "EG",
-                        "ES",
-                        "ES",
-                        "FR",
-                        "GB",
-                        "GR",
-                        "GR",
-                        "GT",
-                        "HK",
-                        "HT",
-                        "HU",
-                        "ID",
-                        "IE",
-                        "IL",
-                        "IN",
-                        "IR",
-                        "IR",
-                        "IR",
-                        "IS",
-                        "IS",
-                        "IT",
-                        "JP",
-                        "KE",
-                        "KH",
-                        "KR",
-                        "LB",
-                        "LB",
-                        "LB",
-                        "LT",
-                        "LU",
-                        "MX",
-                        "NL",
-                        "NO",
-                        "NP",
-                        "NZ",
-                        "PE",
-                        "PH",
-                        "PL",
-                        "PT",
-                        "RO",
-                        "RU",
-                        "RW",
-                        "SE",
-                        "SG",
-                        "SI",
-                        "SV",
-                        "SV",
-                        "TH",
-                        "TR",
-                        "TW",
-                        "UA",
-                        "US",
-                        "VC",
-                        "VI",
-                        "VN"),
-                 iso3=c("UAE",
-                        "AFG",
-                        "ATG",
-                        "AUS",
-                        "AUT",
-                        "BEL",
-                        "BEN",
-                        "BGR",
-                        "BGD",
-                        "BWA",
-                        "BLR",
-                        "CAN",
-                        "CAF",
-                        "CHE",
-                        "CHL",
-                        "CHN",
-                        "CHL",
-                        "CHN",
-                        "COL",
-                        "COG",
-                        "COM",
-                        "COD",
-                        "CRI",
-                        "CUB",
-                        "CYP",
-                        "CZE",
-                        "DEU",
-                        "DNK",
-                        "ECU",
-                        "EST",
-                        "EGY",
-                        "ESP",
-                        "EST",
-                        "FRA",
-                        "GBR",
-                        "GRC",
-                        "GRD",
-                        "GTM",
-                        "HKG",
-                        "HTI",
-                        "HUN",
-                        "IDN",
-                        "IRL",
-                        "ISR",
-                        "IND",
-                        "IRL",
-                        "IRQ",
-                        "IRN",
-                        "ISL",
-                        "ISR",
-                        "ITA",
-                        "JPN",
-                        "KEN",
-                        "KHM",
-                        "KOR",
-                        "LBN",
-                        "LBY",
-                        "LBR",
-                        "LTU",
-                        "LUX",
-                        "MEX",
-                        "NLD",
-                        "NOR",
-                        "NPL",
-                        "NZL",
-                        "PER",
-                        "PHL",
-                        "PLW",
-                        "PRT",
-                        "ROU",
-                        "RUS",
-                        "RWA",
-                        "SEN",
-                        "SGP",
-                        "SVN",
-                        "SVN",
-                        "SVK",
-                        "THA",
-                        "TUR",
-                        "TWN",
-                        "UKR",
-                        "USA",
-                        "VCT",
-                        "VIR",
-                        "VNM"))
+pass<-data.frame(iso2=c("AE","AF","AT","AU","AU",
+                        "BE","BE","BG","BG","BW",
+                        "BY","CA","CA","CH","CH",
+                        "CH","CL","CN","CO","CO",
+                        "CO","CO","CR","CU","CY",
+                        "CZ","DE","DK","EC","EE",
+                        "EG","ES","ES","FR","GB",
+                        "GR","GR","GT","HK","HT",
+                        "HU","ID","IE","IL","IN",
+                        "IR","IR","IR","IS","IS",
+                        "IT","JP","KE","KH","KR",
+                        "LB","LB","LB","LT","LU",
+                        "MX","NL","NO","NP","NZ",
+                        "PE","PH","PL","PT","RO",
+                        "RU","RW","SE","SG","SI",
+                        "SV","SV","TH","TR","TW",
+                        "UA","US","VC","VI","VN"),
+                 iso3=c("UAE","AFG","ATG","AUS","AUT",
+                        "BEL","BEN","BGR","BGD","BWA",
+                        "BLR","CAN","CAF","CHE","CHL",
+                        "CHN","CHL","CHN","COL","COG",
+                        "COM","COD","CRI","CUB","CYP",
+                        "CZE","DEU","DNK","ECU","EST",
+                        "EGY","ESP","EST","FRA","GBR",
+                        "GRC","GRD","GTM","HKG","HTI",
+                        "HUN","IDN","IRL","ISR","IND",
+                        "IRL","IRQ","IRN","ISL","ISR",
+                        "ITA","JPN","KEN","KHM","KOR",
+                        "LBN","LBY","LBR","LTU","LUX",
+                        "MEX","NLD","NOR","NPL","NZL",
+                        "PER","PHL","PLW","PRT","ROU",
+                        "RUS","RWA","SEN","SGP","SVN",
+                        "SVN","SVK","THA","TUR","TWN",
+                        "UKR","USA","VCT","VIR","VNM"))
 #Map
 pass%>%
-  distinct(iso2)-> pass
+  dplyr::distinct(iso2)-> pass
+
 live_by_country %>%
   mutate(iso2 = as.character(live_by_country$country)) %>%
   left_join(pass,by=c("iso2"="iso2"))-> map
@@ -217,6 +82,7 @@ highchart() %>%
   hc_mapNavigation(enabled = TRUE) %>%
   hc_colors(heat.colors(2))
 
+tail("iso3",1) %in% names(map)
 #############################################
 #           Per currency                    #
 #############################################
@@ -308,21 +174,6 @@ highchart() %>%
   hc_title(text = "Kickstarter average amt by country") %>%
   hc_legend(enabled = FALSE)
 
-most_backed %>%
-  group_by(country)%>%
-  summarize(amt= mean(amt.pledged)) %>%
-  arrange(amt)-> live_by_country
-
-
-highchart() %>% 
-  hc_add_series_labels_values(live_by_country$country, 
-                              live_by_country$amt, 
-                              type = "bar",
-                              colorByPoint = TRUE) %>%
-  hc_xAxis(categories = live_by_country$country) %>%
-  hc_title(text = "Kickstarter average amt by country") %>%
-  hc_legend(enabled = FALSE)
-
 #############################################
 #                Per goal                   #
 #############################################
@@ -360,8 +211,52 @@ highchart() %>%
   hc_legend(enabled = FALSE)
 
 #############################################
-#                    Blurb                  #
+#                    Title                  #
 #############################################
-word<-unlist(strsplit(as.character(unlist(live$title[1:10])),split=" "))
-wordcloud(word,freq=rep(1,length(word)))
-live$title[1]
+
+word<-unlist(strsplit(as.character(unlist(live$title)),split=" "))
+docs <- Corpus(VectorSource(word))
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, stripWhitespace)
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 5,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+word<-unlist(strsplit(as.character(unlist(most_backed$title)),split=" "))
+docs <- Corpus(VectorSource(word))
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, stripWhitespace)
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 5,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
